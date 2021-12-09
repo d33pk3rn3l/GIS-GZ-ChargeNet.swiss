8924bab331fbecdf618450b84775956623d8838f
= <=>
2021-12-09 16:20:23 +0100

    started weighing

--- weight.py
+++ weight.py
@@ -1,10 +1,8 @@
-from pyaxis import pyaxis
-fp = "Data/City_Scenarios.px" 
-#parse contents of *.px file
-px = pyaxis.parse(uri = fp , encoding = 'ISO-8859-2')
+import pandas as pd
+from IPython.display import display
 
-#store data as pandas dataframe
-data_df = px['DATA']
-#store metadata as a dictionary (not necessary in this case, but might be helpful)
-meta_dict = px['METADATA']
-print(meta_dict)
+with open("./Data/bevoelkerung.csv") as csv:
+    pop = pd.read_csv(csv, delimiter=";")
+    csv.close()
+
+pop.drop(pop[pop.VARIABLE != "pop2019"].index, inplace=True)
